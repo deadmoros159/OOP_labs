147569b1c53b3ec612f5a4c51d81db55a148d573
deadmoros159 <eeboy159@gmail.com>
2025-11-14 00:17:03 +0300

    Add files via upload

---
+++ laba_1/build/CMakeFiles/main_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_app.dir/func_test.c++.o"
+  "CMakeFiles/main_app.dir/func_test.c++.o.d"
+  "CMakeFiles/main_app.dir/test.c++.o"
+  "CMakeFiles/main_app.dir/test.c++.o.d"
+  "main_app"
+  "main_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ laba_1/build/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/func_test.c++.o"
+  "CMakeFiles/run_tests.dir/func_test.c++.o.d"
+  "CMakeFiles/run_tests.dir/tests.c++.o"
+  "CMakeFiles/run_tests.dir/tests.c++.o.d"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
